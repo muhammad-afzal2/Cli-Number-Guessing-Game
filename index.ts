ab149a6884f65af5942afe2bb767a74567c86b9b
Muhammad Afzal <161644889+muhammad-afzal2@users.noreply.github.com>
2024-04-29 19:14:45 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber: number = Math.floor(Math.random() * 20 + 1);
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Guess a Number between 1-20",
+  },
+]);
+
+// conditional statement
+
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congratulation You Guessed Right Number");
+} else {
+  console.log("You Guessed Wrong Number");
+}
